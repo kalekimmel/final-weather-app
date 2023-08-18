26c2a6f66bcbad1ae47c34842125ae5c84f65455
kalekimmel <KimmelK11@gmail.com>
2023-08-17 20:34:59 -0500

    Final version

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+	  "X-RapidAPI-Key": "8f3666057bmshbabed4daa7f3865p1aa5d8jsn739172d734bf",
+	  "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "90d786890dfce2a3ac39e711f11ceff8"; 
